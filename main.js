a8db14d11a3e678871caa935b38e83e3966af730
Batneto <mdpa23@gmail.com>
2023-02-15 22:20:20 +0100

    update noche

--- main.js
+++ main.js
@@ -1,30 +1,58 @@
-
 // const cartas=document.getElementsByClassName("gridcontainer")
 
 // const selector=document.getElementsByTagName("select")
 // document.addEventListener('DOMContentLoaded'),()=>{
-    
 
-function banner(){
+function banner() {
 
-    const banner = document.getElementById("banner") //todo Por que si la declaro fuera no funciona
+  const banner = document.getElementById("banner"); //todo Por que si la declaro fuera no funciona
 
-    let img = document.createElement("img")
+  let img = document.createElement("img");
 
-     let arrayFotos=["img/banner/2.jpg","img/banner/1.jpg","img/banner/3.jpg","img/banner/4.jpg","img/banner/5.jpg","img/banner/6.jpg","img/banner/7.jpg","img/banner/8.jpg"]
+  let arrayFotos = [
+    "img/banner/2.jpg",
+    "img/banner/1.jpg",
+    "img/banner/3.jpg",
+    "img/banner/4.jpg",
+    "img/banner/5.jpg",
+    "img/banner/6.jpg",
+    "img/banner/7.jpg",
+    "img/banner/8.jpg",
+  ];
 
-        let aleatorio= arrayFotos[Math.floor(Math.random()*arrayFotos.length)];
-        
-        img.src=aleatorio
-    
-    banner.append(img)
+  let aleatorio = arrayFotos[Math.floor(Math.random() * arrayFotos.length)];
 
-} 
-banner()
+  img.src = aleatorio;
+
+  banner.append(img);
+}
 
 
-const cartas = document.getElementsByClassName
 
+
+
+function opciones(){
+let opciones = document.getElementById("opciones");
+const fragment= document.createDocumentFragment();
+
+
+let destinos=["Madrid","Barcelona","Sevilla"];
+
+for(i=0;i<destinos.length;i++){
+
+    let ciudad =document.createElement("option");
+    ciudad.textContent=destinos[i];
+    fragment.append(ciudad);
+}
+
+opciones.append(fragment);
+
+}
+
+
+banner();
+
+opciones()
 
 
 
